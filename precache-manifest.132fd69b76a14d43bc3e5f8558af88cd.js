a355913ca3c10b49dd291232801977816f729f44
rexclement <146126821+rexclement@users.noreply.github.com>
2024-10-03 07:19:19 +0530

    Add files via upload

---
+++ precache-manifest.132fd69b76a14d43bc3e5f8558af88cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6132bdfc8154af6f7c3d002a0665699f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061d3ee178e8b623d3c7",
+    "url": "/static/css/main.9fce249d.chunk.css"
+  },
+  {
+    "revision": "d506632f584249aba5a2",
+    "url": "/static/js/2.128e394c.chunk.js"
+  },
+  {
+    "revision": "061d3ee178e8b623d3c7",
+    "url": "/static/js/main.3c7180ed.chunk.js"
+  },
+  {
+    "revision": "2f55955791534fe0d08b",
+    "url": "/static/js/runtime-main.1048f588.js"
+  }
+]);
